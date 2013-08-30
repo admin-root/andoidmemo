4e89e0b334e27825beca777733614880ac3a0278
shenkaige86 <shenkaige86@gmail.com>
2013-08-30 12:45:34 +0000

    rename needConfigWrapView

--- AndroidMemo/src/com/phodev/android/tools/viewpage/ViewWraperConfig.java
+++ AndroidMemo/src/com/phodev/android/tools/viewpage/ViewWraperConfig.java
@@ -10,7 +10,7 @@ import android.view.ViewGroup;
  * 
  */
 public interface ViewWraperConfig {
-	public boolean needCconfigWrapView();
+	public boolean needConfigWrapView();
 
 	public boolean isWrapViewShowable();
 
